bfb195b418aaa7b45208a1b0ada4faf188a8553e
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2022-07-06 11:27:15 +0700

    fix bug

--- Delivery/Signin.go
+++ Delivery/Signin.go
@@ -2,9 +2,11 @@ package Delivery
 
 import (
 	"net/http"
+
+	"github.com/tieldmoon/tieldauth/Service"
 )
 
-func SigninHandler(w http.ResponseWriter, r *http.Request) {
+func SigninHandler(w http.ResponseWriter, r *http.Request, wo *Service.Worker) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
