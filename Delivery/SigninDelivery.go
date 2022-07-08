3fad2cc12416b5f56df8f8c700e6976c00638704
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 <mohammadarfan45@gmail.com>
2022-07-08 15:54:24 +0700

    server update

--- Delivery/SigninDelivery.go
+++ Delivery/SigninDelivery.go
@@ -2,11 +2,17 @@ package Delivery
 
 import (
 	"net/http"
+
+	"github.com/tieldmoon/tieldauth/Repository"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func SigninHandler(w http.ResponseWriter, r *http.Request) {
+func SigninHandler(w http.ResponseWriter, r *http.Request, mongodb *mongo.Client) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
+	}
+	_ = Repository.TokenRepositoryMongo{
+		Client: mongodb,
 	}
 
 	// m := <-wo.Mongo
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
-	"github.com/tieldmoon/tieldauth/Delivery"
+	"github.com/tieldmoon/tieldauth/Service"
 )
 
 func main() {
@@ -16,9 +17,14 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
+	db := Service.MongoConnPool()
+	defer db.Disconnect(context.TODO())
+
 	// Oauth2
 	r.Group(func(r chi.Router) {
-		r.Post("/api/oauth2/signin", Delivery.SigninHandler)
+		r.Post("/api/oauth2/signin", func(w http.ResponseWriter, r *http.Request) {
+
+		})
 	})
 	port := os.Getenv("PORT")
 	if port == "" {
