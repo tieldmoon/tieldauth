b4de4e4684a32ac0f4811aef435e52b3eb3d35b7
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2022-07-20 08:38:55 +0700

    update progress

--- Repository/UserRepository.go
+++ Repository/UserRepository.go
@@ -1,5 +1,34 @@
 package Repository
 
+import (
+	"context"
+
+	"github.com/tieldmoon/tieldauth/Models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type UserRepository interface {
-	Auth(u string, p string) (bool, error)
+	FindByEmail(email string) (Models.User, bool)
+}
+
+type UserRepositoryMongo struct {
+	Client *mongo.Client
+}
+
+func (u *UserRepositoryMongo) FindByEmail(email string) (Models.User, bool) {
+	col := u.Client.Database("User").Collection("user_data")
+	var result bson.D
+	col.FindOne(context.TODO(), bson.D{}).Decode(&result)
+	doc, err := bson.Marshal(result)
+	if err != nil {
+		return Models.User{}, false
+	}
+	var data Models.User
+	err = bson.Unmarshal(doc, &data)
+	if err != nil {
+		return Models.User{}, false
+	}
+	return data, true
+
 }
